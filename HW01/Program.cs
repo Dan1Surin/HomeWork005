2ef26604cfab8e98b01195db9182104b4c929341
Danil Surin <dan1surin@gmail.com>
2023-01-25 22:54:26 +0300

    1

---
+++ HW01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Домашняя работа:
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop" и производится
+// при помощи нажатия Enter
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+bool B = false;
+int count = 0;
+while (B != true)
+{
+    Console.WriteLine("Your number");
+    string? number = Console.ReadLine();
+    if (number != "stop")
+    {
+        int a = Convert.ToInt32(number);
+        if (a > 0)
+        {
+            count++;
+        }
+    }
+    else B = true;
+}
+Console.WriteLine($"положительных чисел - {count}");
